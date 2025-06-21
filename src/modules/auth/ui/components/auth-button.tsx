fe8299cf04a18cd5dcdf04e93732758a62b099ed
wootaiklee <fined000@naver.com>
2025-06-21 21:03:09 +0900

    feat: clerk auth 추가

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,15 +1,30 @@
+"use client";
+import {
+  UserButton,
+  SignIn,
+  SignUp,
+  SignedOut,
+  SignInButton,
+} from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { UserCircleIcon } from "lucide-react";
 
 export const AuthButton = () => {
   //  TODO: add different button for logged in and logged out users
+
   return (
-    <Button
-      variant="outline"
-      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
-    >
-      <UserCircleIcon className="size-4" />
-      Sign in
-    </Button>
+    <>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
+          >
+            <UserCircleIcon className="size-4" />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };
