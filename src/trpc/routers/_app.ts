321576907c80e7eb002161d012ff92bac224aa6e
wootaiklee <fined000@naver.com>
2025-07-20 21:22:42 +0900

    feat: videos TRPC

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
+import { studioRouter } from "@/modules/studio/server/procedure";
 import { createTRPCRouter } from "../init";
 import { categoriesRouter } from "@/modules/categories/server/procedure";
 
 export const appRouter = createTRPCRouter({
   categories: categoriesRouter,
+  studio: studioRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
