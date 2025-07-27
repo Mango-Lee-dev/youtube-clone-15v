4943358de94334b93f65b398a32ff7b998b9618f
wootaiklee <fined000@naver.com>
2025-07-27 15:05:40 +0900

    feat: studio video table 생성, 무한 스크롤 구현

--- src/modules/studio/ui/view/studio-view.tsx
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -1,9 +1,15 @@
-import { VideoSection } from "../sections/video-section";
+import { VideosSection } from "../sections/video-section";
 
 export const StudioView = () => {
   return (
-    <div>
-      <VideoSection />
+    <div className="flex flex-col gap-y-6 pt-2.5">
+      <div className="px-4">
+        <h1>Channel Contents</h1>
+        <p className="text-xs text-muted-foreground">
+          Manage your channel content and videos
+        </p>
+      </div>
+      <VideosSection />
     </div>
   );
 };
