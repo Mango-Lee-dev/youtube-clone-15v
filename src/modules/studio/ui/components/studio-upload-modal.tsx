301bbadbf4d5b63a1a8740e0a4e1d7464c673c43
wootaiklee <fined000@naver.com>
2025-08-02 22:37:25 +0900

    feat: mux video 연동

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { ResponsiveModal } from "@/components/responsive-modal";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
 import { toast } from "sonner";
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModal = () => {
   const utils = trpc.useUtils();
@@ -12,22 +14,39 @@ export const StudioUploadModal = () => {
       toast.success("비디오가 생성되었습니다.");
       utils.studio.getMany.invalidate();
     },
-    onError: (error) => {
+    onError: (error: Error) => {
       toast.error(error.message);
     },
   });
   return (
-    <Button
-      variant="secondary"
-      onClick={() => create.mutate()}
-      disabled={create.isPending}
-    >
-      {create.isPending ? (
-        <Loader2Icon className="animate-spin" />
-      ) : (
-        <PlusIcon />
-      )}
-      Create
-    </Button>
+    <>
+      <ResponsiveModal
+        open={!!create.data}
+        onOpenChange={() => create.reset()}
+        title="Upload Video"
+      >
+        <p>Uploader</p>
+        {create.data?.url ? (
+          <StudioUploader
+            endpoint={create.data.url}
+            onSuccess={() => create.reset()}
+          />
+        ) : (
+          <Loader2Icon />
+        )}
+      </ResponsiveModal>
+      <Button
+        variant="secondary"
+        onClick={() => create.mutate()}
+        disabled={create.isPending}
+      >
+        {create.isPending ? (
+          <Loader2Icon className="animate-spin" />
+        ) : (
+          <PlusIcon />
+        )}
+        Create
+      </Button>
+    </>
   );
 };
--- src/modules/videos/server/procedure.ts
+++ src/modules/videos/server/procedure.ts
@@ -1,10 +1,19 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
+
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+      },
+      cors_origin: "*", // TODO: production 환경에서는 본인 url로 설정
+    });
 
     const [video] = await db
       .insert(videos)
@@ -15,6 +24,7 @@ export const videosRouter = createTRPCRouter({
       .returning();
     return {
       video: video,
+      url: upload.url,
     };
   }),
 });
