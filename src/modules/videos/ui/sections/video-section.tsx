300642b2bf54b67a9a3c797cb1e515a2f96ff91f
wootaiklee <fined000@naver.com>
2025-09-21 21:06:41 +0900

    feat: videobanner, videoplayer 완성

--- src/modules/videos/ui/sections/video-section.tsx
+++ src/modules/videos/ui/sections/video-section.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { VideoPlayer } from "../components/video-player";
+import { VideoBanner } from "../components/video-banner";
 
 interface VideoSectionProps {
   videoId: string;
@@ -20,5 +23,22 @@ export const VideoSection = ({ videoId }: VideoSectionProps) => {
 
 const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
   const [video] = trpc.videos.getOne.useSuspenseQuery({ id: videoId });
-  return <div>Video Section {video.title}</div>;
+  return (
+    <>
+      <div
+        className={cn(
+          "aspect-video bg-black rounded-xl overflow-hidden relative",
+          video.muxStatus !== "ready" && "rounded-b-none"
+        )}
+      >
+        <VideoPlayer
+          autoPlay
+          onPlay={() => {}}
+          playbackId={video.muxPlaybackId}
+          thumbnailUrl={video.thumbnailUrl}
+        />
+      </div>
+      <VideoBanner status={video.muxStatus} />
+    </>
+  );
 };
