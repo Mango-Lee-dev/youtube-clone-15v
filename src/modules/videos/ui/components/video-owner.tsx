fc00d3b880278f6919f7121e1c1e7bc75b477710
wootaiklee <fined000@naver.com>
2025-09-27 13:52:28 +0900

    feat: UserInfo Tooltip

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -4,6 +4,7 @@ import { VideoGetOneOutput } from "../../types";
 import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
+import { UserInfo } from "@/modules/users/ui/components/user-info";
 
 interface VideoOwnerProps {
   user: VideoGetOneOutput["user"];
@@ -20,9 +21,12 @@ export const VideoOwner = ({ user, videoId }: VideoOwnerProps) => {
       <Link href={`/users/${user.id}`}>
         <div className="flex items-center gap-3 min-w-0">
           <UserAvatar imageUrl={user.imageUrl} name={user.name} size="lg" />
-          <span className="text-sm text-muted-foreground line-clamp-1">
-            {0} subscribers
-          </span>
+          <div className="flex flex-col gap-1 min-w-0">
+            <UserInfo name={user.name} size="lg" />
+            <span className="text-sm text-muted-foreground line-clamp-1">
+              {0} subscribers
+            </span>
+          </div>
         </div>
       </Link>
       {isOwner ? (
