7683bf6398fc468a1cfb56140de3d825766c1db8
wootaiklee <fined000@naver.com>
2025-09-27 14:09:06 +0900

    feat: video-reactions, video-menu 생성

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,5 +1,7 @@
 import { VideoGetOneOutput } from "@/modules/videos/types";
 import { VideoOwner } from "./video-owner";
+import { VideoReactions } from "./video-reactions";
+import { VideoMenu } from "./video-menu";
 
 interface VideoTopRowProps {
   video: VideoGetOneOutput;
@@ -11,6 +13,10 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
       <h1 className="text-xl font-semibold">{video.title}</h1>
       <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4">
         <VideoOwner user={video.user} videoId={video.id} />
+        <div className="flex overflow-x-auto sm:min-w-[calc(50%-6px)] sm:justify-end sm:overflow-visible pb-2 -mb-2 sm:pb-0 sm:mb-0 gap-2">
+          <VideoReactions />
+          <VideoMenu videoId={video.id} variant="secondary" />
+        </div>
       </div>
     </div>
   );
