ae23f99fa02121dee5bf296d314e566a5fada3da
wootaiklee <fined000@naver.com>
2025-10-18 14:39:36 +0900

    feat: comments router 추가

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -179,6 +179,10 @@ export const commentRelations = relations(comments, ({ one }) => ({
   }),
 }));
 
+export const commentInsertSchema = createInsertSchema(comments);
+export const commentUpdateSchema = createUpdateSchema(comments);
+export const commentSelectSchema = createSelectSchema(comments);
+
 export const videoViews = pgTable(
   "video_views",
   {
