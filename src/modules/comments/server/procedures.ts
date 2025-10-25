55fc471917c041bd5192c487c67e4d82c657f7ae
wootaiklee <fined000@naver.com>
2025-10-25 15:10:24 +0900

    feat: remove method 추가, comment join 추가

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -5,10 +5,32 @@ import {
   createTRPCRouter,
   protectedProcedure,
 } from "@/trpc/init";
-import { and, count, eq, getTableColumns, lt, or } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+import { and, count, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 import z from "zod";
 
 export const commentsRouter = createTRPCRouter({
+  remove: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { id } = input;
+      const { id: userId } = ctx.user;
+
+      const [deletedComment] = await db
+        .delete(comments)
+        .where(and(eq(comments.id, id), eq(comments.userId, userId)))
+        .returning();
+
+      if (!deletedComment) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      return deletedComment;
+    }),
   create: protectedProcedure
     .input(
       z.object({
@@ -32,6 +54,7 @@ export const commentsRouter = createTRPCRouter({
     .input(
       z.object({
         videoId: z.string().uuid(),
+        parentId: z.string().uuid().nullish(),
         cursor: z
           .object({
             id: z.string().uuid(),
@@ -69,7 +92,10 @@ export const commentsRouter = createTRPCRouter({
                   )
                 : undefined
             )
-          ),
+          )
+          .innerJoin(users, eq(comments.userId, users.id))
+          .orderBy(desc(comments.updatedAt), desc(comments.id))
+          .limit(limit + 1),
       ]);
 
       const hasMore = data.length > limit;
