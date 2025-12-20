e26ec5c0db186ce974a7468068a67c267bde8126
wootaiklee <fined000@naver.com>
2025-12-20 14:13:37 +0900

    refactor: simplify video list rendering in HomeVideosSection by storing flatMap result in a variable

--- src/modules/home/ui/sections/home-videos-section.tsx
+++ src/modules/home/ui/sections/home-videos-section.tsx
@@ -31,14 +31,13 @@ const HomeVideosSectionSuspense = ({ categoryId }: HomeVideosSectionProps) => {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     }
   );
+  const viedoList = videos.pages.flatMap((page) => page.items);
   return (
     <div>
       <div className="gap-4 gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 [@media(min-width:1920px)]:grid-cols-5 [@media(min-width:2200px)]:grid-cols-6">
-        {videos.pages
-          .flatMap((page) => page.items)
-          .map((video) => (
-            <VideoGridCard key={video.id} data={video} />
-          ))}
+        {viedoList.map((video) => (
+          <VideoGridCard key={video.id} data={video} />
+        ))}
       </div>
       <InfiniteScroll
         hasNextPage={query.hasNextPage}
